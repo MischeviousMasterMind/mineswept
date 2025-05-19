373539d72dacb90d8122dacc88bdb00742fda260
1903806 <1903806@01-18107.pusd.dom>
2025-05-19 13:24:53 -0700

    copy/pasted Chunk class from master branch to Trey branch

--- src/main/java/mineswept/Chunk.java
+++ src/main/java/mineswept/Chunk.java
@@ -1,5 +1,82 @@
 package mineswept;
 
+import java.math.BigInteger;
+
 public class Chunk {
 
+	private Tile[][] tiles;
+	private int numOfTilesSweeped, numOfEmptyTiles, numOfMines;
+	
+	private final int width, height;
+	
+	private BigInteger seed;
+	
+	private Chunk north, south, east, west;
+	
+	// TODO Implement constructor
+	public Chunk(int width, int height, int numOfMines) {
+		
+		this.width = 0;
+		this.height = 0;
+		
+	}
+	
+	public Chunk(int width, int height, int numOfMines, BigInteger seed) {
+		
+		this(width, height, numOfMines);
+		
+	}
+	
+	// TODO Implement constructor
+	public Chunk(Tile[][] tiles) {
+		
+		this.width = 0;
+		this.height = 0;
+		this.tiles = tiles;
+		
+	}
+	
+	public BigInteger numOfConfigurations() {
+		return null;
+	}
+	
+	// TODO Implement method
+	private void generate(int width, int height, int numOfMines) {
+		
+		
+		
+	}
+
+	public Tile getTile(int row, int col) {
+		
+		return tiles[row][col];
+		
+	}
+	
+	public void setTile(int row, int col, Tile newTile) {
+		
+		tiles[row][col] = newTile;
+		
+	}
+	
+	public BigInteger getSeed() {
+		return seed;
+	} 
+
+	public void setAdjacentChunk(Chunk adjacentChunk, CardinalDirection direction) {
+		switch(direction) {
+			case NORTH:
+				north = adjacentChunk;
+				break;
+			case SOUTH:
+				south = adjacentChunk;
+				break;	
+			case WEST:
+				west = adjacentChunk;
+				break;
+			case EAST:
+				east = adjacentChunk;
+				break;		
+		}		
+	}
 }
